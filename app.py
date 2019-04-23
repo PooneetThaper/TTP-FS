614fa0b3916a347fc05d3c36041ce6b987351e61
Pooneet Thaper <pooneetthaper@gmail.com>
2019-04-23 18:12:31 -0400

    Trying to handle form input but request.form is empty for all post requests.

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, redirect, request, render_template, session, url_for
+from werkzeug import generate_password_hash, check_password_hash
 import db
 app = Flask(__name__)
 
 @app.route('/')
-def main():
+def index():
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if 'email' in session:
+        return redirect(url_for("portfolio"))
+    if request.method == 'POST':
+        print(request.form)
+        # authenticate
+        # session
+        # redirect to portfolio
+        return redirect("/login")
+    else:
+        return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if 'email' in session:
+        return redirect(url_for("portfolio"))
+    if request.method == 'POST':
+        print(request.form)
+        # db.create_user(
+        #     request.form['name'],
+        #     request.form['email'],
+        #     request.form['password'])
+        return redirect("/login")
+    else:
+        return render_template('register.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('index.html')
+
+@app.route('/transactions')
+def transactions():
+    return render_template('index.html')
--- db.py
+++ db.py
@@ -13,10 +13,35 @@ class User(Base):
 
      email = Column(String, primary_key=True)
      name = Column(String)
-     Password = Column(String)
+     password_hash = Column(String)
+     cash = Column(Integer)
 
      def __repr__(self):
          return "<User(email='%s', name='%s')>" % (
                 self.email, self.name)
 
+class Holding(Base):
+     __tablename__ = 'holdings'
+
+     email = Column(String, primary_key=True)
+     ticker = Column(String, primary_key=True)
+     quantity = Column(String)
+
+     def __repr__(self):
+         return "<Holding(email='%s', ticker='%s', quantity='%s')>" % (
+                self.email, self.name, self.quantity)
+
 Base.metadata.create_all(engine)
+
+
+def create_user(name, email, password):
+    Session.add(User(
+        email=email,
+        name=name,
+        password_hash=generate_password_hash(password),
+        cash=5000))
+    Session.commit()
+
+def authenticate_user(email, password):
+    user = User.query.filter(email=email)
+    return check_password_hash(user.password_hash, password)
