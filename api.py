30d1bf08102cf7d43e5f4231a22de09e27ae636d
Pooneet Thaper <pooneetthaper@gmail.com>
2019-04-24 01:54:16 -0400

    created functions to wrap the calls to the IEX API for stock info

---
+++ api.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_price(ticker):
+    return requests.get('https://api.iextrading.com/1.0/stock/{}/price'.format(ticker))
+
+def get_open_high_low_close(ticker):
+    return requests.get('https://api.iextrading.com/1.0/stock/{}/ohlc'.format(ticker))
+
+def batch_get_info(tickers, endpoints):
+    return requests.get('https://api.iextrading.com/1.0/stock/market/batch?symbols={}&type='.format(",".join(tickers), ",".join(endpoints)))
