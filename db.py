1ac0da3cb729d27bccbf4acedddb4e1039d3225f
Pooneet Thaper <pooneetthaper@gmail.com>
2019-04-24 01:53:31 -0400

    added functions to get access to various information from the database

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
+from werkzeug import generate_password_hash, check_password_hash
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///:memory:', echo=True)
-
 from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+engine = create_engine('sqlite:///foo.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
 
-from sqlalchemy import Column, Integer, String
 class User(Base):
      __tablename__ = 'users'
 
@@ -17,8 +18,8 @@ class User(Base):
      cash = Column(Integer)
 
      def __repr__(self):
-         return "<User(email='%s', name='%s')>" % (
-                self.email, self.name)
+         return "<User(email='%s', name='%s', cash='%s')>" % (
+                self.email, self.name, self.cash)
 
 class Holding(Base):
      __tablename__ = 'holdings'
@@ -31,17 +32,54 @@ class Holding(Base):
          return "<Holding(email='%s', ticker='%s', quantity='%s')>" % (
                 self.email, self.name, self.quantity)
 
-Base.metadata.create_all(engine)
+class Transaction(Base):
+     __tablename__ = 'transactions'
 
+     email = Column(String, primary_key=True)
+     date_time = Column(String, primary_key=True)
+     type = Column(String)
+     ticker = Column(String)
+     quantity = Column(String)
+
+     def __repr__(self):
+         return "<Transaction(email='%s', date_time='%s', type='%s', ticker='%s', quantity='%s')>" % (
+                self.email, self.date_time, self.type, self.name, self.quantity)
+
+Base.metadata.create_all(engine)
+session.commit()
 
 def create_user(name, email, password):
-    Session.add(User(
-        email=email,
-        name=name,
-        password_hash=generate_password_hash(password),
-        cash=5000))
-    Session.commit()
+    try:
+        session.add(User(
+            email=email.lower(),
+            name=name,
+            password_hash=generate_password_hash(password),
+            cash=5000))
+        session.commit()
+        return True
+    except:
+        return False
 
 def authenticate_user(email, password):
-    user = User.query.filter(email=email)
+    user = session.query(User).filter_by(email=email.lower()).first()
+    if user == None:
+        return False
     return check_password_hash(user.password_hash, password)
+
+def get_user_cash(email):
+    user = session.query(User).filter_by(email=email.lower()).first()
+    if user == None:
+        return False
+    return user.cash
+
+def get_user_holdings(email):
+    holdings = session.query(Holding).filter_by(email=email.lower()).all()
+    if holdings == None:
+        return False
+    return holdings
+
+def get_user_transactions(email):
+    transactions = session.query(Transaction).filter_by(email=email.lower()).all()
+    if transactions == None:
+        return False
+    return transactions
